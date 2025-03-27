a182caf9476d3432a7eb8cd55e967361c8e9aec2
Darrell Tunnell <darrell.tunnell@uniun.io>
2025-03-27 12:12:36 +0000

    Added OTEL companion project.

---
+++ src/DataFlow.OpenTelemetry/MeterProviderBuilderExtensions.cs
@@ -0,0 +1,17 @@
+namespace Uniun.DataFlow.OpenTelemetry
+{
+    using global::OpenTelemetry.Metrics;
+    using Uniun.DataFlow.Metrics;
+
+    // Extension methods must be defined in a static class
+    public static class MeterProviderBuilderExtensions
+    {
+        public static MeterProviderBuilder AddDataFlows(this MeterProviderBuilder builder)
+        {
+            return builder
+                .AddMeter(MeterAccessor.MeterName);
+            // You could also add other meters that your library uses
+            // Or add specific instrumentation setup your library needs
+        }
+    }
+}
